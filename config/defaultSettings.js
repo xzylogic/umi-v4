4a013fe782987b345eec31b7a0fad1c6bc8f23e7
xzylogic <1062186337@qq.com>
2019-10-29 14:34:05 +0800

    1029umi-v4第一次提交

---
+++ config/defaultSettings.js
@@ -0,0 +1,16 @@
+export default {
+  navTheme: 'dark',
+  primaryColor: '#1890FF',
+  layout: 'topmenu',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  autoHideHeader: false,
+  fixSiderbar: false,
+  colorWeak: false,
+  menu: {
+    locale: true,
+  },
+  title: '开发者中心',
+  pwa: false,
+  iconfontUrl: '',
+};
